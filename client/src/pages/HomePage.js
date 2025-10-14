f0c8ad39e13b6a099243436a82f110fdd83f3509
Dominik Szilvasi <dominik.szilvasi.2024@mumail.ie>
2025-10-14 10:51:18 +0100

    new background changing on homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
-
 import Button from '../components/Button';
 
+// 1. Import Swiper components and modules
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade } from 'swiper/modules';
+
+// 2. Import Swiper's core styles and the modules' styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
+
+// Import the images
 import bg1 from '../assets/images/bg1.jpg';
 import bg2 from '../assets/images/bg2.jpg';
 import bg3 from '../assets/images/bg3.jpg';
@@ -15,18 +22,31 @@ const slideImages = [bg1, bg2, bg3, bg4, bg5];
 
 const HomePage = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden"> {/* Use full screen height and width */}
-      <div className="absolute inset-0"> {/* New wrapper for the slider */}
-        <Slide duration={5000} transitionDuration={1000} indicators={false} arrows={false} pauseOnHover={false}>
+    <div className="relative h-screen w-screen overflow-hidden">
+      <div className="absolute inset-0">
+        {/* 3. Configure the Swiper component */}
+        <Swiper
+          modules={[Autoplay, EffectFade]} // Tell Swiper which modules to use
+          effect="fade" // Set the transition effect
+          loop={true} // Loop the slides
+          autoplay={{
+            delay: 5000, // 5 seconds per slide
+            disableOnInteraction: false, // Autoplay continues even after user interaction
+          }}
+          className="h-full w-full"
+        >
           {slideImages.map((slideImage, index) => (
-            <div key={index} className="h-screen w-screen bg-cover bg-center"
-                 style={{ 'backgroundImage': `url(${slideImage})` }}>
-            </div>
+            <SwiperSlide key={index}>
+              <div
+                className="h-full w-full bg-cover bg-center"
+                style={{ backgroundImage: `url(${slideImage})` }}
+              />
+            </SwiperSlide>
           ))}
-        </Slide>
+        </Swiper>
       </div>
 
-      {/* The centered content overlay */}
+      {/* The centered content overlay (this part remains the same) */}
       <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black bg-opacity-50 text-white z-10">
         <h1 className="text-5xl md:text-7xl font-extrabold mb-4 animate-fade-in-down">
           Welcome to GameNightHub
