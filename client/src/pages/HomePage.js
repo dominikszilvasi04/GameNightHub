e118d037882ab7f02107f44a1ca7ff2931b8612f
Dominik Szilvasi <dominik.szilvasi.2024@mumail.ie>
2025-10-14 11:42:13 +0100

    added cleaner slide transition

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -19,8 +19,14 @@ import bg4 from '../assets/images/bg4.jpg';
 import bg5 from '../assets/images/bg5.jpg';
 import bg6 from '../assets/images/bg6.jpg';
 import bg7 from '../assets/images/bg7.jpg';
+import bg8 from '../assets/images/bg8.jpg';
+import bg9 from '../assets/images/bg9.jpg';
+import bg10 from '../assets/images/bg10.jpg';
+import bg11 from '../assets/images/bg11.jpg';
+import bg12 from '../assets/images/bg12.jpg';
+import bg13 from '../assets/images/bg13.jpg';
 
-const slideImages = [bg1, bg2, bg3, bg4, bg5, bg6, bg7];
+const slideImages = [bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9, bg10, bg11, bg12, bg13];
 
 const HomePage = () => {
   return (
@@ -29,13 +35,14 @@ const HomePage = () => {
         {/* 3. Configure the Swiper component */}
         <Swiper
           modules={[Autoplay, EffectFade]} // Tell Swiper which modules to use
-          effect="fade" // Set the transition effect
+          effect="slide" // Set the transition effect
           loop={true} // Loop the slides
           autoplay={{
             delay: 5000, // 5 seconds per slide
             disableOnInteraction: false, // Autoplay continues even after user interaction
           }}
           className="h-full w-full"
+          speed={1200}
         >
           {slideImages.map((slideImage, index) => (
             <SwiperSlide key={index}>
